b353b125b7ec90e1d7ddf03278c144cc7d7fe34a
SE-Sabbir <sabbirhp450@gmail.com>
2025-05-17 22:43:58 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$('.count_num').counterUp({
+    delay: 10,
+    time: 1000
+});
+// ----slider js part------
+
+ $('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: false,
+  centerMode: true,
+  focusOnSelect: true
+});
